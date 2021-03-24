6c3b58291cd54b671e66f6111e022621f8ee23f0
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2021-03-24 15:23:02 -0300

    Create DataTypeAttribute.cs

---
+++ Simple.MySql/Attributes/DataTypeAttribute.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Simple.MySql.Schema;
+
+namespace Simple.MySql.Attributes
+{
+    public class DataTypeAttribute : Attribute
+    {
+        public MySqlDbType Type { get; }
+        public int Len { get; }
+        public bool Unsigned { get; }
+
+        public DataTypeAttribute(MySqlDbType type, int len = 0, bool unsigned = false)
+        {
+            Type = type;
+            Len = len;
+            Unsigned = unsigned;
+        }
+    }
+}
